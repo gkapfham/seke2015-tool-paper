d2642bee4f267dc56b905e8421278c3454614eb2
Cody Kinneer <kinneerc@allegheny.edu>
2015-05-06 18:23:09 -0400

    cranked out a draft

---
+++ content/QuickSortTrimmed.java
@@ -0,0 +1,18 @@
+public class QuickSortDoublingExperiment extends DoublingExperiment{
+    private int size = 10;
+    public static void main(String[] args){
+        SortingExperiment exp = new SortingExperiment();
+        exp.runExperiment();
+        exp.printBigOh();
+    }
+    protected void doubleN(){
+        size *= 2;
+    }
+    protected double timedTest(){
+        int[] n = createInput(size);
+        long startTime = System.nanoTime();
+        QuickSort.quickSort(n, n.length);
+        long endTime = System.nanoTime();
+        return (double) endTime - startTime;
+    }
+}
