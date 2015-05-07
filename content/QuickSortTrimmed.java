ab73ed6a3f2ca2c8e06794c9d8e4b29fe10ea3ba
Cody Kinneer <kinneerc@allegheny.edu>
2015-05-07 17:43:30 -0400

    code golf

--- content/QuickSortTrimmed.java
+++ content/QuickSortTrimmed.java
@@ -1,14 +1,9 @@
-public class QuickSortDoublingExperiment 
-    extends DoublingExperiment{
+public class QuickSortExp extends DoublingExperiment{
     private int size = 10;
     public static void main(String[] args){
         SortingExperiment exp = new SortingExperiment();
-        exp.runExperiment();
-        exp.printBigOh();
-    }
-    protected void doubleN(){
-        size *= 2;
-    }
+        exp.runExperiment(); exp.printBigOh(); }
+    protected void doubleN(){ size *= 2; }
     protected double timedTest(){
         int[] n = createInput(size);
         long startTime = System.nanoTime();
